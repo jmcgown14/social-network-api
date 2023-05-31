de24f0a8bb4ee90e96458d21401d4cf64b0d01f0
Johnny McGown <jmac3292@gmail.com>
2023-05-31 14:50:01 -0600

    created Reaction and updated thoughts

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,60 +1,38 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
 
 const ThoughtSchema = new mongoose.Schema(
   {
     thoughtText: {
       type: String,
-      required: true,
+      required: "What is your thought?",
       minlength: 1,
-      maxlength: 280
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+      get: (timestamp) => dateFormat(timestamp),
     },
     username: {
       type: String,
-      required: true
+      required: true,
     },
-    reactions: [
-      {
-        reactionBody: {
-          type: String,
-          required: true,
-          minlength: 1,
-          maxlength: 280
-        },
-        username: {
-          type: String,
-          required: true
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-          get: timestamp => dateFormat(timestamp)
-        }
-      }
-    ]
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
-      virtuals: true,
-      getters: true
+      getters: true,
     },
-    id: false
+    id: false,
   }
 );
 
 // virtual property that retrieves the length of the 'reactions' array
-ThoughtSchema.virtual('reactionCount').get(function () {
+ThoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const dateFormat = timestamp => {
-  return new Date(timestamp).toLocaleDateString();
-};
-
-const Thought = mongoose.model('Thought', ThoughtSchema);
-
+const Thought = mongoose.model("Thought", ThoughtSchema);
 module.exports = Thought;
